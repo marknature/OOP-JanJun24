7bcae3a476fb162a5aeb22682be68948ffc79a44
Mark Chindudzi <chindudzim@africau.edu>
2024-05-30 19:31:10 +0200

    Add file "Trigonometry in Degrees"

---
+++ Practice Projects in CSharp/Trigonometry in Degrees/Trigonometry in Degrees/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trigonometry_in_Degrees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the degrees: ");
+            double angleDegrees = Convert.ToInt32(Console.ReadLine());
+
+            // Convert degrees to radians
+            double angleRadians = angleDegrees * Math.PI / 180.0;
+
+            // Trigonometric functions
+            double sinValue = Math.Sin(angleRadians);
+            double cosValue = Math.Cos(angleRadians);
+            double tanValue = Math.Tan(angleRadians);
+
+            // Display results
+            Console.WriteLine("For angle " + angleDegrees + " degrees:");
+            Console.WriteLine("Sine: " + sinValue);
+            Console.WriteLine("Cosine: " + cosValue);
+            Console.WriteLine("Tangent: " + tanValue);
+            Console.ReadKey();
+        }
+    }
+}
