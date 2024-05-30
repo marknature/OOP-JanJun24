a9113c430787edbb99ecaf35079b9e48eee93492
Mark Chindudzi <chindudzim@africau.edu>
2024-05-30 19:29:42 +0200

    Add files via upload

---
+++ Practice Projects in CSharp/Trigonometry in Radians/Trigonometry in Radians/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trigonometry_in_Radians
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter 1 for 180 degrees, 2 for 90 degrees, 3 for 60 degrees or 0 for 0 degrees: ");
+            int value;
+            value = Convert.ToInt32(Console.ReadLine());
+
+            double angleRadians = 0;            // 0 degrees in radians
+            if (value <= 3)
+            {
+                if (value == 1)
+                {
+                    angleRadians = Math.PI;     // 180 degrees in radians
+                }
+                else if (value == 2)
+                {
+                    angleRadians = Math.PI / 2; // 90 degrees in radians
+                }
+                else if (value == 3)
+                {
+                    angleRadians = Math.PI / 3; // 60 degrees in radians
+                }
+
+                Console.WriteLine("\nTrigonometric functions in radians:");
+                Console.WriteLine("Angle in radians: " + angleRadians);
+                Console.WriteLine("Sine of " + angleRadians + ": " + Math.Sin(angleRadians));
+                Console.WriteLine("Cosine of " + angleRadians + ": " + Math.Cos(angleRadians));
+                Console.WriteLine("Tangent of " + angleRadians + ": " + Math.Tan(angleRadians));
+                Console.ReadKey();
+            }
+
+            else if(value > 3)
+            {
+                Console.WriteLine("Please enter a number from 1 to 3!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Practice Projects in CSharp/Trigonometry in Radians/Trigonometry in Radians/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Trigonometry in Radians")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("Trigonometry in Radians")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("93847b30-8635-4883-86e1-dd4967ae111f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
