7f12dd2bed500c75b370425828842b150d176e96
Mark Chindudzi <chindudzim@africau.edu>
2024-06-01 09:22:25 +0200

    Added 2 files with pattern making c#

---
+++ Practice Projects in CSharp/Diamond Pattern/Diamond Pattern/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diamond_Pattern
+{
+    class DiamondPattern
+    {
+        static void Main(string[] args)
+        {
+            int n = 5; // Number of rows (should be odd for perfect diamond shape)
+            Console.WriteLine("Diamond Pattern\nwith * and spaces:\n"); // Heading
+
+            // Upper part of the diamond
+            for (int i = 1; i <= n; i += 2)
+            {
+                for (int j = 0; j < (n - i) / 2; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int k = 1; k <= i; k++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+
+            // Lower part of the diamond
+            for (int i = n - 2; i >= 1; i -= 2)
+            {
+                for (int j = 0; j < (n - i) / 2; j++)
+                {
+                    Console.Write(" ");
+                }
+                
+                for (int k = 1; k <= i; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Practice Projects in CSharp/Pascal Triangle/Pascal Triangle/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pascal_Triangle
+{
+    class PascalTriangle
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pascal's Triangle:\n");
+            int numRows = 6; // Number of rows in the triangle
+
+            for (int i = 0; i < numRows; i++)
+            {
+                int number = 1;
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write(number + " ");
+                    number = number * (i - j) / (j + 1);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
